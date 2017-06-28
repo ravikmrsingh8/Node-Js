c61da76b6b6c079d3974add3f00c525edc8b4843
Ravi Kumar Singh <RaviKumar.Singh@jda.com>
2017-06-28 16:56:35 +0530

    Weather Api changed to http://openweathermap.org/

--- weather-app-axios/src/app.js
+++ weather-app-axios/src/app.js
@@ -20,16 +20,18 @@ axios.get(geoCodeUrl).then((response) => {
     var address  = response.data.results[0].formatted_address;
     var lat = response.data.results[0].geometry.location.lat;
     var lng = response.data.results[0].geometry.location.lng;
-    var apiKey = process.env.WEATHER_API_KEY;
-    var weatherInfoUrl = `https://api.darksky.net/forecast/${apiKey}/${lat},${lng}`;
+    var apiKey = process.env.OPEN_WEATHER_KEY;
+
+    //var weatherInfoUrl = `https://api.darksky.net/forecast/${apiKey}/${lat},${lng}`;
+    var weatherInfoUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=${apiKey}`;
     console.log(address);
     return axios.get(weatherInfoUrl);
 
 }).then((response) => {
-    console.log(response.data.currently);
-    var temp = response.data.currently.apparentTemperature;
-    var tempInCelsius = ((temp - 32)*5)/9;
-    console.log("Temperature :",tempInCelsius,"Degree celsius");
+    console.log(response.data);
+    //var temp = response.data.currently.apparentTemperature;
+    //var tempInCelsius = ((temp - 32)*5)/9;
+    //console.log("Temperature :",tempInCelsius,"Degree celsius");
 
 }).catch((error) =>{
     if(error.code === 'ENOTFOUND') {
